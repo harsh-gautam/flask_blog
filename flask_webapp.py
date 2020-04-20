374e8aa9b7638236cc0bb22391f59e7b3701e3cc
Harsh Gautam <hgautam208@gmail.com>
2020-04-20 13:12:19 +0530

    Added register.html and login.html

--- flask_webapp.py
+++ flask_webapp.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '23jbhbh21h1gvbdv12g3vjdgva7atd12hj8asd'
 
 posts = [
     {
@@ -24,6 +26,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
