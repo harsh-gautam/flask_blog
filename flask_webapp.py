7d278b5c20f5a6e2fc8e2ca8f467e28d9bfd62f5
Harsh Gautam <hgautam208@gmail.com>
2020-04-22 13:55:32 +0530

    Added some styling

--- flask_webapp.py
+++ flask_webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '23jbhbh21h1gvbdv12g3vjdgva7atd12hj8asd'
@@ -27,12 +27,15 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created successfully for {form.username.data}', category='success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     return render_template('login.html', title='Login', form=form)
